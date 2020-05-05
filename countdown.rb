fafce1269ded19be265f4801eddcf3b84d033d97
Parker Williamson <parker.williamson@gmail.com>
2020-05-05 17:00:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 
 def countdown
-  countdown = 10 
-  while countdown == 0 
+countdown = 10 
+while countdown > 0  
   puts "#{countdown} SECOND(S)!"
   countdown -= 1 
   return "HAPPY NEW YEAR!"
